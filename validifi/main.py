5d25c9ddaf1fa6cb4f1327221b21bcf1bb35c730
SHOOTDEAD <117431495+SHOOTDEAD@users.noreply.github.com>
2023-05-23 14:26:04 +0530

    Add files via upload

---
+++ validifi/main.py
@@ -0,0 +1,53 @@
+from fastapi import FastAPI,UploadFile,File
+import validifi
+import duckdb
+ 
+app=FastAPI()
+
+@app.post('/validate_Configuration_1')
+async def Validate_Configuration_1(file : UploadFile=File(...)):
+    b_file= await file.read()
+    
+    validation=validifi.c1.verify(file.filename,b_file).func()
+    db=duckdb.connect('logs.db')
+    cursor=db.cursor()
+    if type(validation) == bytes:
+        cursor.execute("insert into logs values(?,?)",(file.filename,1))
+        cursor.commit()
+        cursor.close()
+        return {'status':1,'file':validation}
+    cursor.execute("insert into logs values(?,?)",(file.filename,validation))
+    db.commit()
+    db.close()
+    return {'status':0,'error':validation}
+@app.post('/validate_Configuration_2')
+async def Validate_Configuration_2(file : UploadFile=File(...)):
+    b_file= await file.read()
+    
+    validation=validifi.c2.verify(file.filename,b_file).func()
+    db=duckdb.connect('logs.db')
+    cursor=db.cursor()
+    if type(validation) == bytes:
+        cursor.execute("insert into logs values(?,?)",(file.filename,1))
+        cursor.commit()
+        cursor.close()
+        return {'status':1,'file':validation}
+    cursor.execute("insert into logs values(?,?)",(file.filename,validation))
+    db.commit()
+    db.close()
+    return {'status':0,'error':validation}
+@app.post('/get_Configuration')
+async def Get_Configuration(file : UploadFile=File(...)):
+    pass
+@app.get('/get_Logs')
+def Get_Logs():
+    db = duckdb.connect('logs.db')
+    cursor = db.cursor()
+    File = validifi.get_logs.get_logs().func(cursor)
+    return {'file':File}
+@app.get('/suma')
+def suma():
+    return {'a':'PK\\x03\\x04\\x14\\x00\\x00\\x00\\x08\\x00m'}
+   
+
+    
