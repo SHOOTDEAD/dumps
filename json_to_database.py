5854271bc6ab59b0466f0c0e5ff08d808b594a9e
SHOOTDEAD <117431495+SHOOTDEAD@users.noreply.github.com>
2023-12-09 20:36:38 +0530

    Update json_to_database.py

--- json_to_database.py
+++ json_to_database.py
@@ -16,38 +16,38 @@ with open("jsondata.json",'rb') as file:
 print(file_data[0].keys())
 # columns = dict()
 
-# for i in file_data[0].keys():
-#     if i in ["end_year","intensity","start_year","relevance","likelihood"]:
-#         columns[i] = "int"
-#     else:
-#         columns[i] = "varchar(1000)"
-# # Creating table from json data
-# query = "create table blogs("
-# for name,column_type in columns.items():
-#     query+=f"{name} {column_type},"
-# query = query[:-1] + ")"
-# cursor.execute(query)
+for i in file_data[0].keys():
+    if i in ["end_year","intensity","start_year","relevance","likelihood"]:
+        columns[i] = "int"
+    else:
+        columns[i] = "varchar(1000)"
+# Creating table from json data
+query = "create table blogs("
+for name,column_type in columns.items():
+    query+=f"{name} {column_type},"
+query = query[:-1] + ")"
+cursor.execute(query)
 
-# #inserting json data into the table
-# query =f"insert into blogs ({','.join(file_data[0].keys())}) values({'%s,'*len(file_data[0])}"[:-1] + ");" 
-# query = query[:-1]
-# data = []
-# for i in file_data:
-#     temp_data = list(i.values())
-#     for index,value in enumerate(temp_data):
-#         if value == "":
-#             temp_data[index] = None
+#inserting json data into the table
+query =f"insert into blogs ({','.join(file_data[0].keys())}) values({'%s,'*len(file_data[0])}"[:-1] + ");" 
+query = query[:-1]
+data = []
+for i in file_data:
+    temp_data = list(i.values())
+    for index,value in enumerate(temp_data):
+        if value == "":
+            temp_data[index] = None
 
-#     data.append(temp_data)
-# cursor.executemany(query,data)
+    data.append(temp_data)
+cursor.executemany(query,data)
 
-# connection.commit()
+connection.commit()
 
-# for data in file_data:
-#     for datas in data.values:
-#         query+=f"data"
+for data in file_data:
+    for datas in data.values:
+        query+=f"data"
 
 
-# connection.commit()
+connection.commit()
 
     
